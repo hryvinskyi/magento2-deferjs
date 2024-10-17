ab9a495651a17c7faa643491ad6418d5f017355a
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2024-10-17 15:10:46 +0200

    updated movig logic

--- Model/MoveJsToFooter.php
+++ Model/MoveJsToFooter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2019. Volodymyr Hryvinskyi.  All rights reserved.
+ * Copyright (c) 2019-2024. Volodymyr Hryvinskyi.  All rights reserved.
  * @author: <mailto:volodymyr@hryvinskyi.com>
  * @github: <https://github.com/hryvinskyi>
  */
@@ -14,6 +14,8 @@ use Hryvinskyi\Base\Helper\Json;
 use Hryvinskyi\DeferJs\Helper\Config;
 use Hryvinskyi\DeferJs\Model\Minify\MinifyJsInterface;
 use Hryvinskyi\DeferJs\Model\PassesValidator\ValidateSkipper;
+use Magento\Framework\App\Request\Http as RequestHttp;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Response\Http;
 
 /**
@@ -21,108 +23,100 @@ use Magento\Framework\App\Response\Http;
  */
 class MoveJsToFooter implements MoveJsToFooterInterface
 {
-    /**
-     * @var Config
-     */
-    private $config;
+    private Config $config;
+    private MinifyJsInterface $minifyJs;
+    private ValidateSkipper $validateSkipper;
+    private RequestInterface $request;
 
-    /**
-     * @var MinifyJsInterface
-     */
-    private $minifyJs;
-
-    /**
-     * @var ValidateSkipper
-     */
-    private $validateSkipper;
-
-    /**
-     * MoveJsToFooter constructor.
-     *
-     * @param Config $config
-     * @param MinifyJsInterface $minifyJs
-     * @param ValidateSkipper $validateSkipper
-     */
     public function __construct(
         Config $config,
         MinifyJsInterface $minifyJs,
-        ValidateSkipper $validateSkipper
+        ValidateSkipper $validateSkipper,
+        RequestInterface $request
     ) {
         $this->config = $config;
         $this->minifyJs = $minifyJs;
         $this->validateSkipper = $validateSkipper;
+        $this->request = $request;
     }
 
-    /**
-     * @param Http $http
-     *
-     * @return void
-     */
-    public function execute(Http $http)
+    public function execute(Http $http): void
     {
-        $scripts = [];
         $html = $http->getBody();
 
-        $jsons = [];
-        $scriptStart = '<script';
-        $scriptEnd = '</script>';
-
-        $start = 0;
-        $i = 0;
-        while (($start = stripos($html, $scriptStart, $start)) !== false) {
-            $end = stripos($html, $scriptEnd, $start);
-
-            if ($end === false) {
-                break;
-            }
-
-            $len = $end + strlen($scriptEnd) - $start;
-            $script = substr($html, $start, $len);
-
-            if (
-                $this->config->isOptimizeXMagentoInitScripts() &&
-                strpos($script, 'text/x-magento-init') !== false
-            ) {
-                $jsons[] = strip_tags($script);
-                $html = str_replace($script, '', $html);
-                continue;
-            }
-
-            if ($this->validateSkipper->execute($script, $http)) {
-                $start++;
-                continue;
-            }
-
-            $html = str_replace($script, '', $html);
-            $scripts[] = $script;
-
-            $i++;
+        if (!$this->request instanceof RequestHttp ||  $this->request->isAjax() === true) {
+            return;
         }
+
+        [$scripts, $jsons] = $this->extractScripts($html, $http);
 
         if ($this->config->isMinifyBodyScript()) {
             $scripts = $this->minifyJs->execute($scripts);
         }
 
+        $mergedJson = $this->mergeJsons($jsons);
+        $scripts = implode('', $scripts);
+
+        $html = $this->insertScriptsIntoHtml($html, $mergedJson, $scripts);
+
+        $http->setBody($html);
+    }
+
+    private function extractScripts(string &$html, Http $http): array
+    {
+        $scripts = [];
+        $jsons = [];
+
+        preg_match_all('/<script.*?<\/script>/is', $html, $matches, PREG_OFFSET_CAPTURE);
+        $offsetAdjustment = 0;
+
+        foreach ($matches[0] as [$script, $offset]) {
+            $offset += $offsetAdjustment;
+
+            if ($this->shouldExtractJson($script)) {
+                $jsons[] = strip_tags($script);
+                $html = substr_replace($html, '', $offset, strlen($script));
+                $offsetAdjustment -= strlen($script);
+                continue;
+            }
+
+            if ($this->validateSkipper->execute($script, $http)) {
+                continue;
+            }
+
+            $scripts[] = $script;
+            $html = substr_replace($html, '', $offset, strlen($script));
+            $offsetAdjustment -= strlen($script);
+        }
+
+        return [$scripts, $jsons];
+    }
+
+    private function shouldExtractJson(string $script): bool
+    {
+        return $this->config->isOptimizeXMagentoInitScripts() && preg_match('/<script[^>]*type=["\']text\/x-magento-init["\'][^>]*>/i', $script);
+    }
+
+    private function mergeJsons(array $jsons): string
+    {
         $merged = [];
         foreach ($jsons as $json) {
             $json = Json::decode($json);
             $merged = ArrayHelper::merge($merged, $json);
         }
 
-        if (count($merged) > 0) {
-            $merged = '<script type=text/x-magento-init>' . Json::encode($merged) . '</script>';
-        } else {
-            $merged = '';
+        return count($merged) > 0
+            ? '<script type="text/x-magento-init">' . Json::encode($merged) . '</script>'
+            : '';
+    }
+
+    private function insertScriptsIntoHtml(string $html, string $mergedJson, string $scripts): string
+    {
+        $endBody = stripos($html, '</body>');
+        if ($endBody !== false) {
+            return substr_replace($html, $mergedJson . $scripts, $endBody, 0);
         }
 
-        $scripts = implode('', $scripts);
-
-        if ($endBody = stripos($html, '</body>')) {
-            $html = substr($html, 0, $endBody) . $merged . $scripts . substr($html, $endBody);
-        } else {
-            $html .= $merged . $scripts;
-        }
-
-        $http->setBody($html);
+        return $html . $mergedJson . $scripts;
     }
 }
