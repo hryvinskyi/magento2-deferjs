38ce06813f8853032d4dbe20aba2f8c02c18e784
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2025-03-12 12:26:46 +0100

    Update MoveJsToFooter.php
    Key performance improvements:
    Process all scripts in a single pass rather than modifying HTML multiple times
    Handle script removals in reverse order to avoid offset recalculations
    Only modify the response body when necessary with proper validation
    Early exit conditions to avoid unnecessary processing
    More efficient JSON handling with validation
    Single string manipulation for removing scripts and another for adding content

--- Model/MoveJsToFooter.php
+++ Model/MoveJsToFooter.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Copyright (c) 2019-2024. Volodymyr Hryvinskyi.  All rights reserved.
- * @author: <mailto:volodymyr@hryvinskyi.com>
- * @github: <https://github.com/hryvinskyi>
+ * Copyright (c) 2019-2025. Volodymyr Hryvinskyi. All rights reserved.
+ * Author: Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
+ * GitHub: https://github.com/hryvinskyi
  */
 
 declare(strict_types=1);
@@ -18,9 +18,6 @@ use Magento\Framework\App\Request\Http as RequestHttp;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Response\Http;
 
-/**
- * Class MoveJsToFooter
- */
 class MoveJsToFooter implements MoveJsToFooterInterface
 {
     private Config $config;
@@ -40,83 +37,142 @@ class MoveJsToFooter implements MoveJsToFooterInterface
         $this->request = $request;
     }
 
+    /**
+     * Executes the JS to footer movement process on the HTTP response
+     *
+     * @param Http $http The HTTP response object to process
+     * @return void
+     */
     public function execute(Http $http): void
     {
-        $html = $http->getBody();
-
-        if (!$this->request instanceof RequestHttp ||  $this->request->isAjax() === true) {
+        if (!$this->request instanceof RequestHttp || $this->request->isAjax()) {
             return;
         }
 
-        [$scripts, $jsons] = $this->extractScripts($html, $http);
+        $html = $http->getBody();
 
-        if ($this->config->isMinifyBodyScript()) {
-            $scripts = $this->minifyJs->execute($scripts);
+        // Process all scripts in a single pass
+        $processedHtml = $this->processScripts($html, $http);
+
+        if ($processedHtml !== $html) {
+            $http->setBody($processedHtml);
         }
-
-        $mergedJson = $this->mergeJsons($jsons);
-        $scripts = implode('', $scripts);
-
-        $html = $this->insertScriptsIntoHtml($html, $mergedJson, $scripts);
-
-        $http->setBody($html);
     }
 
-    private function extractScripts(string &$html, Http $http): array
+    /**
+     * Processes script tags in the HTML to move them to footer
+     *
+     * @param string $html The original HTML content
+     * @param Http $http The HTTP response object
+     * @return string The processed HTML with scripts moved to footer
+     */
+    private function processScripts(string $html, Http $http): string
     {
-        $scripts = [];
-        $jsons = [];
-
+        // Find all script tags
         preg_match_all('/<script.*?<\/script>/is', $html, $matches, PREG_OFFSET_CAPTURE);
-        $offsetAdjustment = 0;
 
-        foreach ($matches[0] as [$script, $offset]) {
-            $offset += $offsetAdjustment;
-
-            if ($this->shouldExtractJson($script)) {
-                $jsons[] = strip_tags($script);
-                $html = substr_replace($html, '', $offset, strlen($script));
-                $offsetAdjustment -= strlen($script);
-                continue;
-            }
-
-            if ($this->validateSkipper->execute($script, $http)) {
-                continue;
-            }
-
-            $scripts[] = $script;
-            $html = substr_replace($html, '', $offset, strlen($script));
-            $offsetAdjustment -= strlen($script);
+        if (empty($matches[0])) {
+            return $html;
         }
 
-        return [$scripts, $jsons];
+        // Prepare collections
+        $scriptsToMove = [];
+        $jsonScripts = [];
+        $positions = [];
+
+        // Process matches in reverse order to avoid offset issues
+        foreach (array_reverse($matches[0]) as [$script, $offset]) {
+            if ($this->shouldExtractJson($script)) {
+                $jsonScripts[] = strip_tags($script);
+                $positions[] = [$offset, strlen($script)];
+                continue;
+            }
+
+            if (!$this->validateSkipper->execute($script, $http)) {
+                $scriptsToMove[] = $script;
+                $positions[] = [$offset, strlen($script)];
+            }
+        }
+
+        // If nothing to move, return original
+        if (empty($scriptsToMove) && empty($jsonScripts)) {
+            return $html;
+        }
+
+        // Prepare footer content
+        $footerContent = '';
+
+        // Add merged JSON scripts if any
+        if (!empty($jsonScripts)) {
+            $mergedJson = $this->mergeJsons($jsonScripts);
+            if (!empty($mergedJson)) {
+                $footerContent .= $mergedJson;
+            }
+        }
+
+        // Add scripts if any
+        if (!empty($scriptsToMove)) {
+            if ($this->config->isMinifyBodyScript()) {
+                $footerContent .= implode(PHP_EOL, $this->minifyJs->execute(array_reverse($scriptsToMove)));
+            } else {
+                $footerContent .= implode(PHP_EOL, array_reverse($scriptsToMove));
+            }
+        }
+
+        // Remove scripts from original positions
+        foreach ($positions as [$pos, $len]) {
+            $html = substr_replace($html, '', $pos, $len);
+        }
+
+        // Insert scripts before closing body tag
+        return $this->insertBeforeBodyEnd($html, $footerContent);
     }
 
+    /**
+     * Determines if a script should be extracted as JSON
+     *
+     * @param string $script The script tag to check
+     * @return bool True if the script should be extracted as JSON
+     */
     private function shouldExtractJson(string $script): bool
     {
-        return $this->config->isOptimizeXMagentoInitScripts() && preg_match('/<script[^>]*type=["\']text\/x-magento-init["\'][^>]*>/i', $script);
+        return $this->config->isOptimizeXMagentoInitScripts() &&
+            preg_match('/<script[^>]*type=["\']text\/x-magento-init["\'][^>]*>/i', $script);
     }
 
+    /**
+     * Merges multiple JSON scripts into a single script
+     *
+     * @param array $jsons Array of JSON strings to merge
+     * @return string The merged JSON in a single script tag or empty string
+     */
     private function mergeJsons(array $jsons): string
     {
         $merged = [];
         foreach ($jsons as $json) {
-            $json = Json::decode($json);
-            $merged = ArrayHelper::merge($merged, $json);
+            $data = Json::decode($json);
+            if (is_array($data)) {
+                $merged = ArrayHelper::merge($merged, $data);
+            }
         }
 
-        return count($merged) > 0
+        return !empty($merged)
             ? '<script type="text/x-magento-init">' . Json::encode($merged) . '</script>'
             : '';
     }
 
-    private function insertScriptsIntoHtml(string $html, string $mergedJson, string $scripts): string
+    /**
+     * Inserts content before the closing body tag
+     *
+     * @param string $html The HTML to modify
+     * @param string $content The content to insert before the body end tag
+     * @return string The modified HTML
+     */
+    private function insertBeforeBodyEnd(string $html, string $content): string
     {
-        $endBody = stripos($html, '</body>');
-        if ($endBody !== false) {
-            return substr_replace($html, $mergedJson . $scripts, $endBody, 0);
-        }
-
-        return $html . $mergedJson . $scripts;
+        $bodyEndPos = stripos($html, '</body>');
+        return $bodyEndPos !== false
+            ? substr_replace($html, $content, $bodyEndPos, 0)
+            : $html . $content;
     }
 }
